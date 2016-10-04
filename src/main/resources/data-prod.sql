4ec9e672028f380f62e61d36042b88792f9300e9
Muhammad Hewedy <mhewedy@gmail.com>
2016-10-04 23:34:46 +0300

    prepare for heroku

--- src/main/resources/data-prod.sql
+++ src/main/resources/data-prod.sql
@@ -1,2 +1,5 @@
 
--- TODO init prod admin user
+INSERT INTO "users" (id, created_by, created_date, modified_by, modified_date, enabled, password, role, username)
+  SELECT nextval('hibernate_sequence'), -1, NOW(), -1, NOW(), TRUE, '$2a$10$53dXuhNlKSbXJ6cvFls/jeG2gemezsdk85OHfWVEmmEVnGuMG7i5S', 'ROLE_ADMIN', 'admin'
+WHERE NOT EXISTS
+  (SELECT id FROM "users" WHERE username='admin');
